c43180b5e7e17bd6f8155ccb247495d37e6941a2
Albano <Albano Xhafaj>
2024-08-26 16:39:35 +0200

    refactor: used gen1 scripts for google functions

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -4,4 +4,4 @@
 [[ ! -z "$PRE" ]] && { $PRE "$@"; }
 [[ ! -d ./dist ]] && { echo "Please build the function first"; exit 1; }
 
-gcloud functions deploy "$1" --gen2 --runtime=nodejs20 --source=./dist --entry-point=main --trigger-http --allow-unauthenticated
+gcloud functions deploy "$1" --no-gen2 --runtime=nodejs20 --source=./dist --entry-point=main --trigger-http --allow-unauthenticated
